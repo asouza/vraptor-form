2a09f72a3860d826ad5dbffefbcce500c1130af1
leonardowolter <leocwolter@gmail.com>
2014-01-28 15:42:48 -0200

    removing unnecessary generics and reflecting annotation correctly

--- src/main/java/br/com/vraptorstack/form/ObjectContent.java
+++ src/main/java/br/com/vraptorstack/form/ObjectContent.java
@@ -3,21 +3,23 @@ package br.com.vraptorstack.form;
 import net.vidageek.mirror.dsl.Mirror;
 import br.com.vraptorstack.form.annotation.DefaultValue;
 
-public class ObjectContent<T> {
+public class ObjectContent {
 
 	private String field;
-	private T object;
+	private Object object;
 	private Object content;
 
-	public ObjectContent(T object, String field) {
+	public ObjectContent(Object object, String field) {
 		this.object = object;
 		this.field = field;
 		this.content = new Mirror().on(object).invoke().getterFor(field);
 	}
 	
-	public ObjectContent<T> orDefault() {
-		if(content == null)
-			content = ((DefaultValue) new Mirror().on(object.getClass()).reflect().annotation(DefaultValue.class)).value();
+	public ObjectContent orDefault() {
+		if(content == null) {
+			DefaultValue annotation = new Mirror().on(object.getClass()).reflect().annotation(DefaultValue.class).atField(field);
+			content = annotation.value();
+		}
 		return this;
 	}
 
